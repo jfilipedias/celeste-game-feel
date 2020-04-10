dfc7789b5b9c5309e1e34b7557d4b20560b8d190
JFilipeDias <filipediascontato@gmail.com>
2020-04-10 12:24:32 -0300

    Initial commit

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    [SerializeField] private float minGroundNormalY = 0.65f;
+    [SerializeField] private float gravityModifier = 1f;
+
+    protected bool grounded;
+    protected bool movingVerticaly; 
+    protected Vector2 groundNormal;
+    protected Vector2 velocity;
+    protected Rigidbody2D rb2D;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+
+    private void OnEnable()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+
+    private void FixedUpdate()
+    {
+        CalculateGravity();
+        CalculateMovement();
+    }
+
+
+    private void CalculateGravity()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        grounded = false;
+    }
+
+
+    private void CalculateMovement()
+    {
+        movingVerticaly = true;
+
+        Vector2 verticalMovement = CalculateVertivalMovement();
+      //Vector2 horizontalMovement = CalculateHorizontalMovement();
+        
+        rb2D.position += verticalMovement;
+    }
+
+
+    private Vector2 CalculateVertivalMovement()
+    {
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 vMovement = Vector2.up * deltaPosition.y;
+
+        CheckGroundCollision(vMovement);
+
+        return vMovement;
+
+    }
+
+
+    private void CalculateHorizontalMovement()
+    {
+
+    }
+
+
+    private void CheckGroundCollision(Vector2 direction)
+    {
+        float distance = direction.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+            int count = rb2D.Cast(direction, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+
+            foreach (RaycastHit2D hit in hitBuffer)
+            {
+                hitBufferList.Add(hit);
+            }
+
+            foreach (RaycastHit2D hit in hitBufferList)
+            {
+                Vector2 currentNormal = hit.normal;
+                
+                if(currentNormal.y > minGroundNormalY)
+                {
+                    grounded = true;
+
+                    if (movingVerticaly)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                   
+                if(projection < 0)
+                {
+                    velocity -= projection * currentNormal;     
+                }
+
+                float modifiedDistance = hit.distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Attributies
+
+    [Header("Movement")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float speedIncreaser;
+    [SerializeField] private float jumpForce;
+
+    [Header("Check Collision")]
+    [SerializeField] private Transform playerFoot;
+    [SerializeField] private float gizmoSize;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask groundMask;
+
+    private float movementDirection;
+    private float speed;
+
+    private bool isJumping = false;
+    private bool isGrounded = false;
+    
+    private Rigidbody2D playerRigidbody2D;
+    #endregion
+
+
+    #region Engine Methods
+    private void Awake()
+    {
+        playerRigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (isJumping && isGrounded)
+            Jump();
+        else
+            CheckGroundCollision();
+
+        if (movementDirection != 0)
+            Move();
+        else
+            ResetSpeed();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(playerFoot.position, gizmoSize);
+    }
+    #endregion
+
+
+    #region Controller Methods
+    private void HandleInput()
+    {
+        // Horizontal movement
+        movementDirection = Input.GetAxisRaw("Horizontal");
+       
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            isJumping = true;
+    }
+
+    
+    private void Move()
+    {
+        SmoothSpeed();
+
+        float newVelocityX = movementDirection * speed;
+
+        playerRigidbody2D.velocity =  new Vector2(newVelocityX, playerRigidbody2D.velocity.y);
+    }
+
+
+    private void Jump()
+    {
+        Debug.Log("Jump");
+        isJumping = false;
+        isGrounded = false;
+
+        playerRigidbody2D.velocity += Vector2.up * jumpForce;
+    }
+
+
+    private void SmoothSpeed()
+    {
+        if (speed < maxSpeed)
+            speed *= speedIncreaser;
+        else
+            speed = maxSpeed;
+    }
+
+
+    private void ResetSpeed()
+    {
+        speed = 1f;
+        playerRigidbody2D.velocity = new Vector2(0, playerRigidbody2D.velocity.y);
+    }
+
+
+    private void CheckGroundCollision()
+    {
+        Vector2 rayStart = (Vector2)playerFoot.position;
+
+        isGrounded = Physics2D.Raycast(rayStart, Vector2.down, groundDistance, groundMask);
+    }
+    #endregion
+}
