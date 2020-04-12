35b720616212fd6d8a2eb5581509bc5fad743f02
JFilipeDias <filipediascontato@gmail.com>
2020-04-12 18:12:05 -0300

    Fix ground collision check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,13 +13,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float dashForce;
 
     [Header("Check Collision")]
-    [SerializeField] private float gizmoSize;
     [SerializeField] private float groundCollisionDistance;
     [SerializeField] private float wallCollisionDistance;
+    [SerializeField] private float sphereGizmoSize;
     [SerializeField] private LayerMask groundMask;
-    [SerializeField] private Transform playerFace;
     [SerializeField] private Transform playerHand;
-    [SerializeField] private Transform playerFoot;
+    [SerializeField] private Transform playerKnee;
+    [SerializeField] private Transform playerRightFoot;
+    [SerializeField] private Transform playerLeftFoot;
 
     private Rigidbody2D playerRigidbody2D;
     private SpriteRenderer playerSprite;
@@ -29,7 +30,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 facingDirection = Vector2.right;
 
-    private bool isGrounded = false;
+    private bool isOnGround = false;
     private bool isOnWall = false;
     private bool isFacingWall = false;
     private bool isJumping = false;
@@ -57,18 +58,21 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Jump
-        if (isJumping && isGrounded)
-            Jump();
-        else
+        // Check Colision
+        if(!isOnGround)
             CheckGroundCollision();
-        
-        // Run
-        Run();
-
-        //Climb
+    
         CheckWallCollistion();
-        
+
+        // Move
+        if (!isClimbing)
+            Move();
+
+        // Jump
+        if (isJumping && isOnGround)
+            Jump();
+
+        //Climb        
         if (isClimbing)
             Climb();
         else
@@ -81,7 +85,8 @@ public class PlayerController : MonoBehaviour
         if (isDashing)
             Dash();
 
-        if (isGrounded && !isClimbing && playerRigidbody2D.velocity.y != 0)
+        // Reset Y Velocity
+        if (isOnGround && !isClimbing && playerRigidbody2D.velocity.y != 0)
             ResetVerticalVelocity();
     }
 
@@ -91,17 +96,19 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.magenta;
 
         // Face
-        Gizmos.DrawWireSphere(playerFace.position, gizmoSize);
-        Gizmos.DrawLine(playerFace.position, (playerFace.position + new Vector3(wallCollisionDistance * facingDirection.x, 0, 0)));
+        Gizmos.DrawWireSphere(playerKnee.position, sphereGizmoSize);
+        Gizmos.DrawLine(playerKnee.position, (playerKnee.position + new Vector3(wallCollisionDistance * facingDirection.x, 0, 0)));
         
         // Hand
-        Gizmos.DrawWireSphere(playerHand.position, gizmoSize);
+        Gizmos.DrawWireSphere(playerHand.position, sphereGizmoSize);
         Gizmos.DrawLine(playerHand.position, (playerHand.position + new Vector3(wallCollisionDistance * facingDirection.x, 0, 0)));
         
         // Foot
-        Gizmos.DrawWireSphere(playerFoot.position, gizmoSize);
+        Gizmos.DrawWireSphere(playerRightFoot.position, sphereGizmoSize);
+        Gizmos.DrawWireSphere(playerLeftFoot.position, sphereGizmoSize);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(playerFoot.position, (playerFoot.position + new Vector3(0, groundCollisionDistance * -1, 0)));
+        Gizmos.DrawLine(playerRightFoot.position, (playerRightFoot.position + new Vector3(0, groundCollisionDistance * -1, 0)));
+        Gizmos.DrawLine(playerLeftFoot.position, (playerLeftFoot.position + new Vector3(0, groundCollisionDistance * -1, 0)));
     }
     #endregion
 
@@ -116,19 +123,20 @@ public class PlayerController : MonoBehaviour
         verticalMovementDirection = Input.GetAxisRaw("Vertical");
        
         // Jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump") && isOnGround)
             isJumping = true;
 
         // Climb
-        if (Input.GetButton("Hold") && isWall)
+        if (Input.GetButton("Hold") && isOnWall)
             isClimbing = true;
         else
             isClimbing = false;
     }
 
     
-    private void Run()
+    private void Move()
     {
+        Debug.Log("Move");
         float newVelocityX = horizontalMovementDirection * moveSpeed;
 
         playerRigidbody2D.velocity =  new Vector2(newVelocityX, playerRigidbody2D.velocity.y);
@@ -137,15 +145,17 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        isJumping = false;
-        isGrounded = false;
-
         playerRigidbody2D.velocity += Vector2.up * jumpForce;
+        
+        isJumping = false;
+        isOnGround = false;
     }
 
 
     private void Climb()
     {
+        isOnGround = false;
+
         float newVelocityY = verticalMovementDirection * climbSpeed;
 
         playerRigidbody2D.bodyType = RigidbodyType2D.Kinematic;
@@ -173,8 +183,14 @@ public class PlayerController : MonoBehaviour
 
     private void CheckGroundCollision()
     {
-        Vector2 rayStart = (Vector2)playerFoot.position;
-        isGrounded = Physics2D.Raycast(rayStart, Vector2.down, groundCollisionDistance, groundMask);
+        Vector2 rightRayStart = (Vector2)playerRightFoot.position;
+        Vector2 leftRayStart = (Vector2)playerLeftFoot.position;
+
+        bool rightFootOnGround = Physics2D.Raycast(rightRayStart, Vector2.down, groundCollisionDistance, groundMask);
+        bool leftFootOnGround = Physics2D.Raycast(leftRayStart, Vector2.down, groundCollisionDistance, groundMask);
+
+        isOnGround = rightFootOnGround || leftFootOnGround ? true : false;
+
     }
 
 
@@ -183,7 +199,7 @@ public class PlayerController : MonoBehaviour
         Vector2 handRayStart = (Vector2)playerHand.position;
         isOnWall = Physics2D.Raycast(handRayStart, facingDirection, wallCollisionDistance, groundMask);
 
-        Vector2 faceRayStart = (Vector2)playerFace.position;
+        Vector2 faceRayStart = (Vector2)playerKnee.position;
         isFacingWall = Physics2D.Raycast(faceRayStart, facingDirection, wallCollisionDistance, groundMask);
     }
 
