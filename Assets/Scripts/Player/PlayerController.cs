616bc6a117a3b3ed09d46a06b820b3d3b109b216
Filipe Dias <filipediascontato@gmail.com>
2020-06-22 23:51:02 -0300

    Fix the sudden stop of movement after the dash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Timers")]
     [SerializeField] private float dashTime = 0.2f;
+    [SerializeField] private float dashElapsedTime = 0.1f;
     [SerializeField] private float wallJumpTime = 0.3f;
     [SerializeField] private float jumpBufferingTime = 0.15f;
     [SerializeField] private float coyoteTime = 0.15f;
@@ -45,6 +46,7 @@ public class PlayerController : MonoBehaviour
     private float jumpBufferingCounter;
     private float coyoteTimeCounter;
     private float storedVelocityY;
+    private float dashElapsedCounter;
 
     private int callingCount;
     
@@ -68,6 +70,7 @@ public class PlayerController : MonoBehaviour
     private bool isCornerCorrection;
 
     private bool wasUnground = false;
+    private bool wasDashing = false;
 
     private bool canMove = true;
     private bool canJump = true;
@@ -132,6 +135,7 @@ public class PlayerController : MonoBehaviour
 
         jumpBufferingCounter -= Time.deltaTime;
         coyoteTimeCounter -= Time.deltaTime;
+        dashElapsedCounter -= Time.deltaTime;
     }
 
     private void FixedUpdate()
@@ -207,6 +211,8 @@ public class PlayerController : MonoBehaviour
 
         if (isWallJumping)
             rb2D.velocity = Vector2.Lerp(rb2D.velocity, new Vector2(newVelocityX, rb2D.velocity.y), 5 * Time.deltaTime);
+        else if(dashElapsedCounter > 0)     // Just finish dash
+            rb2D.velocity = Vector2.Lerp(rb2D.velocity, new Vector2(newVelocityX, rb2D.velocity.y), 10 * Time.deltaTime);
         else
             rb2D.velocity = new Vector2(newVelocityX, rb2D.velocity.y);
     }
@@ -369,15 +375,16 @@ public class PlayerController : MonoBehaviour
 
         if (directionY > 0)
             rb2D.velocity *= 0.5f;
-        else
-            rb2D.velocity *= 0.8f;
-        
+
         rb2D.gravityScale = defaultGravityScale;
+
+        dashElapsedCounter = dashElapsedTime;
 
         isDashing = false;
         canMove = true;
         canClimb = true;
         canWallJump = true;
+        wasDashing = true;
     
         if(isOnGround)
             canDash = true;
